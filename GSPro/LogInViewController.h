78bee2bffea9afa740a273c59e869b6f4bac6262
Becky Abramowitz <Becky@Becky-Abramowitzs-MacBook-Pro.local>
2014-05-27 14:05:10 -0700

    Initial commit from local git

---
+++ GSPro/LogInViewController.h
@@ -0,0 +1,16 @@
+//
+//  LogInViewController.h
+//  GSPro
+//
+//  Created by Becky Abramowitz on 5/16/14.
+//  Copyright (c) 2014 Gatorbotics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@property (nonatomic,weak) IBOutlet UITextField *usernameField;
+@property (nonatomic,weak) IBOutlet UITextField *passwordField;
+
+@end
---
+++ GSPro/LogInViewController.m
@@ -0,0 +1,56 @@
+//
+//  LogInViewController.m
+//  GSPro
+//
+//  Created by Becky Abramowitz on 5/16/14.
+//  Copyright (c) 2014 Gatorbotics. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(IBAction)textChanged:(id)sender {
+    [self.usernameField resignFirstResponder];
+    [self.passwordField resignFirstResponder];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
